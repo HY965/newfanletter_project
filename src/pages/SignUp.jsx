347924f810877876043dca5966611e165bd60101
HY965 <hyeonj965@gmail.com>
2024-02-19 20:44:34 +0900

    feat: mypage 추가

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,24 +1,49 @@
 import React from "react";
+import styled from "styled-components";
 
-const SignUp = () => {
+function SignUp() {
   return (
     <>
       <div>
-        <article>
+        <CardWrapper>
           <h2>회원가입</h2>
-          <form>
+          <CardForm>
             <input type="text" placeholder="아이디를 입력해주세요" />
             <input type="password" placeholder="비밀번호를 입력해주세요" />
             <input type="text" placeholder="닉네임을 입력해주세요" />
-
             <button>회원가입</button>
-          </form>
-        </article>
-
-        <div>로그인</div>
+          </CardForm>
+          <div>로그인</div>
+        </CardWrapper>
       </div>
     </>
   );
-};
+}
 
 export default SignUp;
+
+const CardWrapper = styled.article`
+  width: 500px;
+  height: 50vh;
+  background-color: #cfcfcf;
+  padding: 1.5rem;
+  & h2 {
+    font-weight: bold;
+    font-size: 25px;
+    margin-bottom: 40px;
+  }
+`;
+
+const CardForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  & input {
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    border-bottom: 1px solid gray;
+    outline: none;
+  }
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,6 +2,7 @@ import Home from "../pages/Home";
 import Detail from "../pages/Detail";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import Mypage from "../pages/Mypage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import fakeData from "shared/fakeData.json";
 import { useState } from "react";
@@ -21,6 +22,7 @@ export default function Router() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/mypage" element={<Mypage />} />
         {/* :id ->  path parameter url에서의 변수같은 역활  보통 prams라고 부름 */}
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
