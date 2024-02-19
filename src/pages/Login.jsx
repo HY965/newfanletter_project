adf8017cfeb6678d36daf86febe9ffbd30d2b788
HY965 <hyeonj965@gmail.com>
2024-02-19 21:08:37 +0900

    feat: Login 내용 수정, signup 삭제

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
 
 function Login() {
-  return <div>Login</div>;
+  const [changeState, setChangeState] = useState(false);
+
+  const loginOnClickHandler = () => {
+    if (!changeState) {
+      setChangeState(true);
+      return;
+    }
+    setChangeState(false);
+  };
+  return (
+    <>
+      <div>
+        {changeState ? (
+          <CardWrapper>
+            <h2>회원가입</h2>
+            <CardForm>
+              <input type="text" placeholder="아이디를 입력해주세요" />
+              <input type="password" placeholder="비밀번호를 입력해주세요" />
+              <input type="text" placeholder="닉네임을 입력해주세요" />
+              <button>회원가입</button>
+            </CardForm>
+            <span onChange={loginOnClickHandler}>로그인</span>
+          </CardWrapper>
+        ) : (
+          <CardWrapper>
+            <h2>로그인</h2>
+            <CardForm>
+              <input type="text" placeholder="아이디를 입력해주세요" />
+              <input type="password" placeholder="비밀번호를 입력해주세요" />
+              <button>로그인</button>
+            </CardForm>
+            <span>회원가입</span>
+          </CardWrapper>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Login;
+
+const CardWrapper = styled.article`
+  width: 500px;
+  height: 50vh;
+  background-color: #cfcfcf;
+  padding: 1.5rem;
+  & h2 {
+    font-weight: bold;
+    font-size: 25px;
+    margin-bottom: 40px;
+  }
+`;
+
+const CardForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  & input {
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    border-bottom: 1px solid gray;
+    outline: none;
+  }
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,6 @@
 import Home from "../pages/Home";
 import Detail from "../pages/Detail";
 import Login from "../pages/Login";
-import SignUp from "../pages/SignUp";
 import Mypage from "../pages/Mypage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import fakeData from "shared/fakeData.json";
@@ -21,7 +20,6 @@ export default function Router() {
           element={<Detail letter={letter} setLetter={setLetter} />}
         />
         <Route path="/login" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
         <Route path="/mypage" element={<Mypage />} />
         {/* :id ->  path parameter url에서의 변수같은 역활  보통 prams라고 부름 */}
         <Route path="*" element={<Navigate replace to="/" />} />
